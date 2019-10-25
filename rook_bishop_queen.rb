9fdd1bcc37f57983b110a4deb3efdebcb178120a
DreamzRunzLA <salmaan.a.qadir@gmail.com>
2019-10-24 22:09:33 -0600

    End of library sesh

--- rook_bishop_queen.rb
+++ rook_bishop_queen.rb
@@ -4,14 +4,26 @@ require_relative('piece')
 class Rook < Piece
     include Slideable
 
+    def move_dirs
+        return 'hv'
+    end
+
 end
 
 class Bishop < Piece
     include Slideable
 
+    def move_dirs
+        return 'd'
+    end
+
 end
 
 class Queen < Piece
     include Slideable
+
+    def move_dirs
+        return 'b'
+    end
 
 end
--- slideable.rb
+++ slideable.rb
@@ -1,3 +1,15 @@
 module Slideable
 
+    def moves
+        if self.move_dirs == 'hv'
+
+        elsif self.move_dirs == 'd'
+
+        elsif self.move_dirs == 'b'
+
+        else
+            raise 'move direction error'
+        end
+    end
+
 end
