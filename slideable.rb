cbdd2d62295df81396a38c776b4bf6ed5fcd16c5
DreamzRunzLA <salmaan.a.qadir@gmail.com>
2019-10-25 10:59:46 -0600

    Made progress on slideable

--- slideable.rb
+++ slideable.rb
@@ -1,4 +1,7 @@
 module Slideable
+    HORIZONTAL_DIRS = [[0,1],[0,-1],[1,0],[-1,0]]
+    DIAGONOL_DIRS = [[1,1],[1,-1],[-1,-1],[-1,1]]
+    BOTH_DIRS = [[0,1],[0,-1],[1,0],[-1,0],[1,1],[1,-1],[-1,-1],[-1,1]]
 
     def moves
         if self.move_dirs == 'hv'
@@ -10,6 +13,21 @@ module Slideable
         else
             raise 'move direction error'
         end
+    end
+
+    def move_dirs
+    end
+
+    def horizontal_dirs
+        HORIZONTAL_DIRS
+    end
+
+    def diagonol_dirs
+        DIAGONOL_DIRS
+    end
+
+    def both_dirs
+        BOTH_DIRS
     end
 
 end
